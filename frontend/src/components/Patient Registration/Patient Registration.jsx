9e1c912ae1f3d69bf43e9dc34963fdb4d53c3620
DivyaKumar5161 <divyakumar768800@gmail.com>
2023-12-23 23:23:55 +0530

    Revert "modification1"
    This reverts commit 593efd63542d10f4d15646b34831791b94652262.

--- frontend/src/components/Patient Registration/Patient Registration.jsx
+++ frontend/src/components/Patient Registration/Patient Registration.jsx
@@ -1,35 +1,5 @@
-import { generatePath } from "react-router";
 import "./Patient Registration.css";
-import { useState,useRef } from "react";
 export default function () {
-  const patientId = useRef();
-  const firstName = useRef();
-  const middleName = useRef();
-  const lastName = useRef();
-  const dateOfBirth = useRef();
-  const age = useRef();
-  const gender = useRef();
-  const phone = useRef();
-  const email = useRef();
-  const country = useRef();
-  const region = useRef();
-  const address = useRef();
-  const [data, setData] = useState({});
-  function handleSave(){
-    const patientIdValue = patientId.current.value;
-    const firstNameValue = firstName.current.value;
-    const middleNameValue = middleName.current.value;
-    const lastNameValue = lastName.current.value;
-    const dateOfBirthValue = dateOfBirth.current.value;
-    const ageValue = age.current.value;
-    const genderValue=gender.current.value;
-    const phoneValue = phone.current.value;
-    const emailValue = email.current.value;
-    const countryValue = country.current.value;
-    const regionValue = region.current.value;
-    const addressValue = address.current.value;
-    console.log(middleName);
-  }
   return (
     <div className="patient">
       <form action="#" method="post">
@@ -37,42 +7,42 @@ export default function () {
         <div className="patientForm">
           <span className="patientForm1">
             <label for="pid">Patient ID:</label>
-            <input ref={patientId} type="number" id="pid" name="pid" required />
+            <input type="number" id="pid" name="pid" required />
             <label for="fname">First Name:</label>
-            <input ref={firstName}type="text" id="fname" name="fname" required />
-            <label  for="lname">Middle Name:</label>
-            <input ref={middleName} type="text" id="lname" name="lname" required />
+            <input type="text" id="fname" name="fname" required />
+            <label for="lname">Middle Name:</label>
+            <input type="text" id="lname" name="lname" required />
             <label for="lname">Last Name:</label>
-            <input ref={lastName} type="text" id="lname" name="lname" />
+            <input type="text" id="lname" name="lname" />
 
             <label for="dob">Date of Birth:</label>
-            <input ref={dateOfBirth} type="date" id="dob" name="dob" required />
+            <input type="date" id="dob" name="dob" required />
 
             <label for="age">Age:</label>
-            <input ref={age} type="number" id="age" name="age" required />
+            <input type="number" id="age" name="age" required />
           </span>
 
           <span className="patientForm2">
             <label for="gender">Gender:</label>
-            <select ref={gender} id="gender" name="gender" required>
+            <select id="gender" name="gender" required>
               <option value="male">Male</option>
               <option value="female">Female</option>
               <option value="other">Other</option>
             </select>
             <label for="phone">Phone:</label>
-            <input ref={phone} type="tel" id="phone" name="phone" required />
+            <input type="tel" id="phone" name="phone" required />
             <label for="email">Email:</label>
-            <input ref={email} type="email" id="email" name="email" required />
+            <input type="email" id="email" name="email" required />
             <label for="address">Country:</label>
-            <input ref={country} type="text" id="country" name="country" required />
+            <input type="text" id="country" name="country" required />
             <label for="address">Region:</label>
-            <input ref={region} type="text" id="Region" name="Region" required />
+            <input type="text" id="Region" name="Region" required />
             <label for="address">Address:</label>
-            <textarea ref={address} type="textarea" id="address" name="address" required />
+            <textarea type="textarea" id="address" name="address" required />
           </span>
         </div>
         <span id="button">
-        <button type="submit" onClick={handleSave}>Submit</button>
+        <button type="submit">Submit</button>
         <button type="submit">Reset</button>
         </span>
       </form>
