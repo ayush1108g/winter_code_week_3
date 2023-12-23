31f44fb09311fb4bbcee32eddc1dc6f1cd423dea
DivyaKumar5161 <divyakumar768800@gmail.com>
2023-12-23 23:23:36 +0530

    Revert "Merge branch 'main' of https://github.com/ayush1108g/winter_code_week_3"
    This reverts commit 7d4bb40530a10d752b9856c3b3fc63ea94606127, reversing
    changes made to 593efd63542d10f4d15646b34831791b94652262.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,39 @@ import { AnimatePresence } from "framer-motion";
 import classes from "./App.module.css";
 import Errorpage from "./pages/Errorpage";
 import SideBar from "./components/SideBar";
-function App() {
+import HomePage from "./pages/HomePage";
+import EmployeeList from "./pages/EmployeeList";
+import Employeeregistration from "./components/Employee/Employeeregistration";
+import PatientRegistration from "./components/Patient Registration/Patient Registration";
 
-    const screenWidth = window.screen.width;
+
+function RoutesWithAnimation() {
+    const location = useLocation();
+    console.log(location);
     return (
-        <div className="App">
-        <h1>React App</h1>
-        <SideBar />
-        </div>
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.key}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/employeeRegistration" element={<Employeeregistration />} />
+                <Route path="/employeeList" element={<EmployeeList />} />
+                <Route path="*" element={<Errorpage />} />
+            </Routes>
+        </AnimatePresence>
     );
-};
+}
+
+function App() {
+    return (
+        <HashRouter>
+            <div style={{ position: "fixed", width: '18rem', backgroundColor: 'black' }}>
+                <SideBar />
+                {/* <PatientRegistration></PatientRegistration> */}
+            </div>
+            <div className={`h1 d-flex justify-content-center ${classes.gradient}`} style={{ width: '100vw', height: 'auto', padding: '10px' }}> Hospital Data Management</div>
+            <div className="" style={{ marginLeft: '18rem', height: '100dvh' }}>
+                <RoutesWithAnimation />
+            </div>
+  </HashRouter>  
+);
+}
 export default App;
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -3,15 +3,15 @@ import { useNavigate } from "react-router";
 export default function SideBar() {
   const navigate = useNavigate();
   return (
-    <aside className="px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl" style={{ height: '100dvh', overflowY: 'auto' }}>
+    <aside className="px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl" style={{ height: '100dvh' }}>
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
       <div>
         <ul>
           <li><Button onClick={() => { navigate('/') }}> Welcome</Button></li>
-          <li><Button onClick={() => { navigate('/patientRegistration') }}>Patient Registration</Button></li>
           <li><Button> ADD PROJECT</Button></li>
           <li><Button onClick={() => { navigate('/employeeRegistration') }}> Employee Registration</Button></li>
           <li><Button onClick={() => { navigate('/employeeList') }}> Employee List </Button></li>
+          <li><Button> ADD PROJECT</Button></li>
           <li><Button> ADD PROJECT</Button></li>
           <li><Button> ADD PROJECT</Button></li>
         </ul>
