103d06e59e68849b522fe31498d62bfc9782f0d4
DivyaKumar5161 <divyakumar768800@gmail.com>
2023-12-24 00:53:28 +0530

    payment form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes, useLocation, HashRouter } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
-
-import classes from "./App.module.css";
 // import BackImg from "./store/backimg.jpg";
 // import BackImg2 from "./store/backimg2.jpg";
 import Errorpage from "./pages/Errorpage";
 import SideBar from "./components/SideBar";
 import HomePage from "./pages/HomePage";
 import EmployeeList from "./pages/EmployeeList";
-import EmployeeRegistration from "./components/Employee/Employeeregistration";
-import Navbar from "./components/Navbar";
+import Employeeregistration from "./components/Employee/Employeeregistration";
 import PatientRegistration from "./components/Patient Registration/Patient Registration";
 
 
@@ -22,7 +19,7 @@ function RoutesWithAnimation() {
         <AnimatePresence mode="wait">
             <Routes location={location} key={location.key}>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/employeeRegistration" element={<EmployeeRegistration />} />
+                <Route path="/employeeRegistration" element={<Employeeregistration />} />
                 <Route path="/employeeList" element={<EmployeeList />} />
                 <Route path="/patientRegistration" element={<PatientRegistration />} />
                 <Route path="*" element={<Errorpage />} />
@@ -37,10 +34,7 @@ function App() {
         <HashRouter>
             <div className="d-none d-lg-block" style={{ position: "fixed", width: '18rem', backgroundColor: 'black' }}>
                 <SideBar />
-            </div>
-            <div className={`h1 d-flex justify-content-center ${classes.gradient}`} style={{ width: '100vw', height: 'auto', padding: '10px', zIndex: 10 }}> Hospital Data Management</div>
-            <div className="navbar-expand-lg d-lg-none">
-                <Navbar />
+                {/* <PatientRegistration></PatientRegistration> */}
             </div>
             <div className="" style={{ marginLeft: screenWidth > 992 ? '18rem' : '', height: '100dvh' }}>
                 <RoutesWithAnimation />
---
+++ frontend/src/components/Payment/payment.jsx
@@ -0,0 +1,59 @@
+import "./payment.css";
+export default function () {
+  return (
+    <div>
+      <h1>Payment</h1>
+      <form action="">
+        <span className="payForm1">
+          <h3>Employee Information</h3>
+          <label for="pid">Employee ID:</label>
+          <input type="number" id="pid" name="pid" required />
+          <label for="fname">First Name:</label>
+          <input type="text" id="fname" name="fname" required />
+          <label for="lname">Middle Name:</label>
+          <input type="text" id="lname" name="lname" required />
+          <label for="lname">Last Name:</label>
+          <input type="text" id="lname" name="lname" />
+
+          <label for="age">Age:</label>
+          <input type="number" id="age" name="age" required />
+
+          <label for="gender">Gender:</label>
+          <select id="gender" name="gender" required>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          <label for="jobType">Job Type:</label>
+          <select id="jobType" name="jobType" required>
+            <option value="Doctor">Doctor</option>
+            <option value="Nurse">Nurse</option>
+            <option value="other">Other</option>
+          </select>
+          <label for="payment">Payment (in )</label>
+          <input type="number" id="payment" name="payment" required />
+        </span>
+      </form>
+      <form action="">
+        <span>
+          <h3>Payment Information</h3>
+          <label htmlFor="">Attendance</label>
+          <input type="number" />
+          <label htmlFor="">Deduction</label>
+          <input type="number" />
+          <label htmlFor="">OverTime</label>
+          <input type="number" />
+          <label htmlFor="">OverTime Payment</label>
+          <input type="number" />
+          <label htmlFor="">Sub Total</label>
+          <input type="number" />
+          <label htmlFor="">Tax</label>
+          <input type="number" />
+          <label htmlFor="">Total</label>
+          <input type="number" />
+          <button> Total</button>
+        </span>
+      </form>
+    </div>
+  );
+}
