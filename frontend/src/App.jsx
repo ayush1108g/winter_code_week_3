4176de79cd424b5a415ef21f628983fe227a85cb
DivyaKumar5161 <divyakumar768800@gmail.com>
2023-12-23 23:28:33 +0530

    Revert "Merge branch 'main' of https://github.com/ayush1108g/winter_code_week_3"
    This reverts commit 7d4bb40530a10d752b9856c3b3fc63ea94606127, reversing
    changes made to 593efd63542d10f4d15646b34831791b94652262.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,39 @@ import { AnimatePresence } from "framer-motion";
 // import BackImg2 from "./store/backimg2.jpg";
 import Errorpage from "./pages/Errorpage";
 import SideBar from "./components/SideBar";
+import HomePage from "./pages/HomePage";
+import EmployeeList from "./pages/EmployeeList";
+import Employeeregistration from "./components/Employee/Employeeregistration";
+import PatientRegistration from "./components/Patient Registration/Patient Registration";
+
+
+function RoutesWithAnimation() {
+    const location = useLocation();
+    console.log(location);
+    return (
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.key}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/employeeRegistration" element={<Employeeregistration />} />
+                <Route path="/employeeList" element={<EmployeeList />} />
+                <Route path="*" element={<Errorpage />} />
+            </Routes>
+        </AnimatePresence>
+    );
+}
+
 function App() {
     return (
-        <div className="App">
-        <h1>React App</h1>
-        <SideBar />
-        </div>
-    );
-};
+        <HashRouter>
+            <div style={{ position: "fixed", width: '18rem', backgroundColor: 'black' }}>
+                <SideBar />
+                {/* <PatientRegistration></PatientRegistration> */}
+            </div>
+            <div className={`h1 d-flex justify-content-center ${classes.gradient}`} style={{ width: '100vw', height: 'auto', padding: '10px' }}> Hospital Data Management</div>
+            <div className="" style={{ marginLeft: '18rem', height: '100dvh' }}>
+                <RoutesWithAnimation />
+            </div>
+  </HashRouter>  
+);
+}
 export default App;
