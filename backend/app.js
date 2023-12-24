b2e3ff2a3e5bb9f56d6a96ec80365a43250bb097
Pushkar Gupta <22ee01018@iitbbs.ac.in>
2023-12-24 12:54:59 +0530

    #4

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 const employeeroute = require('./routes/Employeeroute')
-
+const patientroute = require('./routes/patientroute')
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -13,5 +13,5 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use('/', employeeroute);
-
+app.use('/',patientroute)
 module.exports = app;
