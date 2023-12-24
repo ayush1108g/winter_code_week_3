97c9e9e0325f443ed5ae695c9aa4ff6555c237bd
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2023-12-24 23:35:11 +0530

    update errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes, useLocation, HashRouter } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import Modal from './UI/Modal'
+// import Modal from './UI/Modal'
 // import BackImg from "./store/backimg.jpg";
 // import BackImg2 from "./store/backimg2.jpg";
 // import classes from "./App.module.css";
